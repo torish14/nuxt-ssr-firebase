e5b5618c2e3732eef74a5a091353ffce7a8d6864
toshihirokato <nanamin.104@icloud.com>
2020-03-27 18:56:27 +0900

    functions の設定ファイル

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   response.send("こんにちは!");
+// });
+
+const nuxtServer = require('./nuxt-server')
+
+exports.nuxtServer = functions.https.onRequest(nuxtServer)
